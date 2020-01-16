software-mansion/react-native-reanimated

commit c97e235cf720ffb7dcaa0b30ec3882772457352e

Use in AnimatedValue.setValue react method instead of evaluateOnce. (#513)
* export setValue method

* ota check

* Update NodesManager.java

* Update REANodesManager.m

Co-authored-by: Michał Osadnik <micosa97@gmail.com>

android/src/main/java/com/swmansion/reanimated/NodesManager.java
@@ -407,4 +407,9 @@ public class NodesManager implements EventDispatcherListener {
   public void sendEvent(String name, WritableMap body) {
     mEventEmitter.emit(name, body);
   }
+
+  public void setValue(int nodeID, Double newValue) {
+    Node node = mAnimatedNodes.get(nodeID);
+    ((ValueNode) node).setValue(newValue);
+  }
 }

android/src/main/java/com/swmansion/reanimated/ReanimatedModule.java
@@ -213,4 +213,14 @@ public class ReanimatedModule extends ReactContextBaseJavaModule implements
       }
     });
   }
+
+  @ReactMethod
+  public void setValue(final int nodeID, final Double newValue) {
+    mOperations.add(new UIThreadOperation() {
+      @Override
+      public void execute(NodesManager nodesManager) {
+        nodesManager.setValue(nodeID, newValue);
+      }
+    });
+  }
 }

ios/REAModule.m
@@ -134,6 +134,14 @@ RCT_EXPORT_METHOD(configureProps:(nonnull NSArray<NSString *> *)nativeProps
   }];
 }
 
+RCT_EXPORT_METHOD(setValue:(nonnull NSNumber *)nodeID
+                  newValue:(nonnull NSNumber *)newValue
+                  )
+{
+  [self addOperationBlock:^(REANodesManager *nodesManager) {
+    [nodesManager setValueForNodeID:nodeID value:newValue];
+  }];
+}
 
 #pragma mark -- Batch handling
 

ios/REANodesManager.h
@@ -74,4 +74,6 @@ typedef void (^REANativeAnimationOp)(RCTUIManager *uiManager);
 
 - (void)dispatchEvent:(id<RCTEvent>)event;
 
+- (void)setValueForNodeID:(nonnull NSNumber *)nodeID value:(nonnull NSNumber *)newValue;
+
 @end

ios/REANodesManager.m
@@ -394,4 +394,16 @@
   _nativeProps = nativeProps;
 }
 
+- (void)setValueForNodeID:(nonnull NSNumber *)nodeID value:(nonnull NSNumber *)newValue
+{
+  RCTAssertParam(nodeID);
+
+  REANode *node = _nodes[nodeID];
+
+  RCTAssertParam(node);
+
+  REAValueNode *valueNode = (REAValueNode *)node;
+  [valueNode setValue:newValue];
+}
+
 @end

src/core/AnimatedValue.js
@@ -1,8 +1,9 @@
 import { createAnimatedSet as set } from '../core/AnimatedSet';
 import interpolate from '../derived/interpolate';
 import InternalAnimatedValue from './InternalAnimatedValue';
-import { evaluateOnce } from '../derived/evaluateOnce';
 import { Platform } from 'react-native';
+import { evaluateOnce } from '../derived/evaluateOnce';
+import  ReanimatedModule from '../ReanimatedModule';
 
 // Animated value wrapped with extra methods for omit cycle of dependencies
 export default class AnimatedValue extends InternalAnimatedValue {
@@ -11,7 +12,13 @@ export default class AnimatedValue extends InternalAnimatedValue {
     if (Platform.OS === 'web') {
       this._updateValue(value);
     } else {
-      evaluateOnce(set(this, value), this);
+      if (ReanimatedModule.setValue) {
+        // FIXME Remove it after some time
+        // For OTA-safety
+        ReanimatedModule.setValue(this.__nodeID, value);
+      } else {
+        evaluateOnce(set(this, value), this);
+      }
     }
   }
   

mccoyplayer/animation

commit 7e61d09f00c40a51199b35a1df5a5e52879602c1

fix for crashing on Android [Problem with setValue when a node is not registered yet] (#560)
The bug was introduced by https://github.com/software-mansion/react-native-reanimated/pull/513

android/src/main/java/com/swmansion/reanimated/NodesManager.java
@@ -402,6 +402,8 @@ public class NodesManager implements EventDispatcherListener {
 
   public void setValue(int nodeID, Double newValue) {
     Node node = mAnimatedNodes.get(nodeID);
-    ((ValueNode) node).setValue(newValue);
+    if (node != null) {
+        ((ValueNode) node).setValue(newValue);
+    }
   }
 }