software-mansion/react-native-reanimated

commit 8638cdbd55f3250bc6d25588e27bb0f2ee44b603

Don't run observer callbacks during sync uimanager batch update (#1501)
## Description

This PR fixes the way we perform sync updates on iOS by avoiding UIManager observers from firing. This in turn fixes the problem of ui manager commands that may be executed out of order as reported in #1498. The reason behind that issue was that due to the fact we'd trigger ui manager observers, those may enqueue some commands in the ui-manager block and we'd run them along the sync ui updates that we were expecting to get after layout phase. In the sync layout call we only expect the ui commands to come from the layout calculations and not from other sources (e.g. native animated or reanimated which uses ui manager observers to enqueue additional commands).

## Changes

1) This PR wraps uimanager batchDidComplete call with additional code that temporarily removes registered ui observers and only puts a single observer instead that is then used to capture mounting block.
2) Some additional cleanup in NodesManager on iOS to extract code that deals with RCTUIManager implementation details to a category on RCTUIManager.
3) As a part of this we also make an update to Android where we trigger semaphore release earlier when sync layout is not possible to match the implementation on iOS.
4) Opt-out of synchronous updates when no reanimated 2 code is being used by adding a flag `trySynchronousBatch` which is only set for reanimated 2 code paths. Then use this flag in the update procedure and skip semaphor wait on iOS and Android when it isn't set

### Before

Under certain condition (difficult to reproduce on a small app) reanimated 1 commands would sometimes run out of order (e.g. first "connect node" and only then "create node")

### After

We no longer allow for reanimated or native animated modules to enqueue commands that are goign to be executed in the sync layout phase.

## Test code and steps to reproduce

android/src/main/java/com/swmansion/reanimated/NodesManager.java
@@ -136,6 +136,7 @@ public class NodesManager implements EventDispatcherListener {
     }
   }
   private Queue<NativeUpdateOperation> mOperationsInBatch = new LinkedList<>();
+  private boolean mTryRunBatchUpdatesSynchronously = false;
 
   public NodesManager(ReactContext context) {
     mContext = context;
@@ -195,13 +196,18 @@ public class NodesManager implements EventDispatcherListener {
     if (!mOperationsInBatch.isEmpty()) {
       final Queue<NativeUpdateOperation> copiedOperationsQueue = mOperationsInBatch;
       mOperationsInBatch = new LinkedList<>();
+      final boolean trySynchronously = mTryRunBatchUpdatesSynchronously;
+      mTryRunBatchUpdatesSynchronously = false;
       final Semaphore semaphore = new Semaphore(0);
       mContext.runOnNativeModulesQueueThread(
               // FIXME replace `mContext` with `mContext.getExceptionHandler()` after RN 0.59 support is dropped
               new GuardedRunnable(mContext) {
                 @Override
                 public void runGuarded() {
-                  boolean shouldDispatchUpdates = UIManagerReanimatedHelper.isOperationQueueEmpty(mUIImplementation);
+                  boolean shouldDispatchUpdates = trySynchronously && UIManagerReanimatedHelper.isOperationQueueEmpty(mUIImplementation);
+                  if (!shouldDispatchUpdates) {
+                    semaphore.release();
+                  }
                   while (!copiedOperationsQueue.isEmpty()) {
                     NativeUpdateOperation op = copiedOperationsQueue.remove();
                     ReactShadowNode shadowNode = mUIImplementation.resolveShadowNode(op.mViewTag);
@@ -211,16 +217,18 @@ public class NodesManager implements EventDispatcherListener {
                   }
                   if (shouldDispatchUpdates) {
                     mUIImplementation.dispatchViewUpdates(-1); // no associated batchId
+                    semaphore.release();
                   }
-                  semaphore.release();
                 }
               });
-      while (true) {
-        try {
-          semaphore.acquire();
-          break;
-        } catch (InterruptedException e) {
-          //noop
+      if (trySynchronously) {
+        while (true) {
+          try {
+            semaphore.acquire();
+            break;
+          } catch (InterruptedException e) {
+            //noop
+          }
         }
       }
     }
@@ -398,7 +406,10 @@ public class NodesManager implements EventDispatcherListener {
     ((PropsNode) node).disconnectFromView(viewTag);
   }
 
-  public void enqueueUpdateViewOnNativeThread(int viewTag, WritableMap nativeProps) {
+  public void enqueueUpdateViewOnNativeThread(int viewTag, WritableMap nativeProps, boolean trySynchronously) {
+    if (trySynchronously) {
+      mTryRunBatchUpdatesSynchronously = true;
+    }
     mOperationsInBatch.add(new NativeUpdateOperation(viewTag, nativeProps));
   }
 
@@ -544,7 +555,7 @@ public class NodesManager implements EventDispatcherListener {
                 viewTag, new ReactStylesDiffMap(newUIProps));
       }
       if (hasNativeProps) {
-        enqueueUpdateViewOnNativeThread(viewTag, newNativeProps);
+        enqueueUpdateViewOnNativeThread(viewTag, newNativeProps, true);
       }
       if (hasJSProps) {
         WritableMap evt = Arguments.createMap();

android/src/main/java/com/swmansion/reanimated/nodes/PropsNode.java
@@ -129,7 +129,7 @@ public class PropsNode extends Node implements FinalNode {
                 mDiffMap);
       }
       if (hasNativeProps) {
-        mNodesManager.enqueueUpdateViewOnNativeThread(mConnectedViewTag, nativeProps);
+        mNodesManager.enqueueUpdateViewOnNativeThread(mConnectedViewTag, nativeProps, false);
       }
       if (hasJSProps) {
         WritableMap evt = Arguments.createMap();

ios/Nodes/REAPropsNode.m
@@ -72,7 +72,7 @@
        props:uiProps];
     }
     if (nativeProps.count > 0) {
-      [self.nodesManager enqueueUpdateViewOnNativeThread:_connectedViewTag viewName:_connectedViewName nativeProps:nativeProps];
+      [self.nodesManager enqueueUpdateViewOnNativeThread:_connectedViewTag viewName:_connectedViewName nativeProps:nativeProps trySynchronously:NO];
     }
     if (jsProps.count > 0) {
       [self.nodesManager.reanimatedModule

ios/REANodesManager.h
@@ -35,7 +35,8 @@ typedef void (^REAEventHandler)(NSString *eventName, id<RCTEvent> event);
 - (void)registerEventHandler:(REAEventHandler)eventHandler;
 - (void)enqueueUpdateViewOnNativeThread:(nonnull NSNumber *)reactTag
                                viewName:(NSString *) viewName
-                            nativeProps:(NSMutableDictionary *)nativeProps;
+                            nativeProps:(NSMutableDictionary *)nativeProps
+                       trySynchronously:(BOOL)trySync;
 - (void)getValue:(REANodeID)nodeID
         callback:(RCTResponseSenderBlock)callback;
 

ios/REANodesManager.m
@@ -40,9 +40,51 @@
 
 @end
 
-@interface REANodesManager() <RCTUIManagerObserver>
+@interface RCTUIManager (SyncUpdates)
 
-@property BOOL shouldInterceptMountingBlock;
+- (BOOL)hasEnqueuedUICommands;
+
+- (void)runSyncUIUpdatesWithObserver:(id<RCTUIManagerObserver>)observer;
+
+@end
+
+@implementation RCTUIManager (SyncUpdates)
+
+- (BOOL)hasEnqueuedUICommands
+{
+  // Accessing some private bits of RCTUIManager to provide missing functionality
+  return [[self valueForKey:@"_pendingUIBlocks"] count] > 0;
+}
+
+- (void)runSyncUIUpdatesWithObserver:(id<RCTUIManagerObserver>)observer
+{
+  // before we run uimanager batch complete, we override coordinator observers list
+  // to avoid observers from firing. This is done because we only want the uimanager
+  // related operations to run and not all other operations (including the ones enqueued
+  // by reanimated or native animated modules) from being scheduled. If we were to allow
+  // other modules to execute some logic from this sync uimanager run there is a possibility
+  // that the commands will execute out of order or that we intercept a batch of commands that
+  // those modules may be in a middle of (we verify that batch isn't in progress for uimodule
+  // but can't do the same for all remaining modules)
+
+  // store reference to the observers array
+  id oldObservers = [self.observerCoordinator valueForKey:@"_observers"];
+
+  // temporarily replace observers with a table conatining just nodesmanager (we need
+  // this to capture mounting block)
+  NSHashTable<id<RCTUIManagerObserver>> *soleObserver = [NSHashTable new];
+  [soleObserver addObject:observer];
+  [self.observerCoordinator setValue:soleObserver forKey:@"_observers"];
+
+  // run batch
+  [self batchDidComplete];
+  // restore old observers table
+  [self.observerCoordinator setValue:oldObservers forKey:@"_observers"];
+}
+
+@end
+
+@interface REANodesManager() <RCTUIManagerObserver>
 
 @end
 
@@ -58,6 +100,7 @@
   BOOL _processingDirectEvent;
   NSMutableArray<REAOnAnimationCallback> *_onAnimationCallbacks;
   NSMutableArray<REANativeAnimationOp> *_operationsInBatch;
+  BOOL _tryRunBatchUpdatesSynchronously;
   REAEventHandler _eventHandler;
   volatile void (^_mounting)(void);
 }
@@ -75,18 +118,12 @@
     _wantRunUpdates = NO;
     _onAnimationCallbacks = [NSMutableArray new];
     _operationsInBatch = [NSMutableArray new];
-    _shouldInterceptMountingBlock = NO;
-    [[uiManager observerCoordinator] addObserver:self];
   }
     
   _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(onAnimationFrame:)];
   [_displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
   [_displayLink setPaused:true];
   return self;
-}
-
-- (void)dealloc {
-  [[_uiManager observerCoordinator] removeObserver:self];
 }
 
 - (void)invalidate
@@ -181,11 +218,9 @@
 }
 
 - (BOOL)uiManager:(RCTUIManager *)manager performMountingWithBlock:(RCTUIManagerMountingBlock)block {
-  if (_shouldInterceptMountingBlock) {
-    _mounting = block;
-    return YES;
-  }
-  return NO;
+  RCTAssert(_mounting == nil, @"Mouting block is expected to not be set");
+  _mounting = block;
+  return YES;
 }
 
 - (void)performOperations
@@ -196,6 +231,9 @@
   if (_operationsInBatch.count != 0) {
     NSMutableArray<REANativeAnimationOp> *copiedOperationsQueue = _operationsInBatch;
     _operationsInBatch = [NSMutableArray new];
+
+    BOOL trySynchronously = _tryRunBatchUpdatesSynchronously;
+    _tryRunBatchUpdatesSynchronously = NO;
     
     __weak typeof(self) weakSelf = self;
     dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
@@ -204,10 +242,9 @@
       if (strongSelf == nil) {
         return;
       }
-      NSMutableArray *pendingUIBlocks = [strongSelf.uiManager valueForKey:@"_pendingUIBlocks"];
-      bool canPerformLayout = ([pendingUIBlocks count] == 0);
+      BOOL canUpdateSynchronously = trySynchronously && ![strongSelf.uiManager hasEnqueuedUICommands];
       
-      if (!canPerformLayout) {
+      if (!canUpdateSynchronously) {
         dispatch_semaphore_signal(semaphore);
       }
       
@@ -215,16 +252,16 @@
         copiedOperationsQueue[i](strongSelf.uiManager);
       }
       
-      if (canPerformLayout) {
-        strongSelf.shouldInterceptMountingBlock = YES;
-        [strongSelf.uiManager batchDidComplete];
-        strongSelf.shouldInterceptMountingBlock = NO;
+      if (canUpdateSynchronously) {
+        [strongSelf.uiManager runSyncUIUpdatesWithObserver:self];
         dispatch_semaphore_signal(semaphore);
       } else {
         [strongSelf.uiManager setNeedsLayout];
       }
     });
-    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+    if (trySynchronously) {
+      dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+    }
     
     if (_mounting) {
       _mounting();
@@ -236,7 +273,11 @@
 
 - (void)enqueueUpdateViewOnNativeThread:(nonnull NSNumber *)reactTag
                                viewName:(NSString *) viewName
-                            nativeProps:(NSMutableDictionary *)nativeProps {
+                            nativeProps:(NSMutableDictionary *)nativeProps
+                       trySynchronously:(BOOL)trySync {
+  if (trySync) {
+    _tryRunBatchUpdatesSynchronously = YES;
+  }
   [_operationsInBatch addObject:^(RCTUIManager *uiManager) {
     [uiManager updateView:reactTag viewName:viewName props:nativeProps];
   }];
@@ -506,7 +547,7 @@
      props:uiProps];
     }
     if (nativeProps.count > 0) {
-      [self enqueueUpdateViewOnNativeThread:viewTag viewName:viewName nativeProps:nativeProps];
+      [self enqueueUpdateViewOnNativeThread:viewTag viewName:viewName nativeProps:nativeProps trySynchronously:YES];
     }
     if (jsProps.count > 0) {
       [self.reanimatedModule sendEventWithName:@"onReanimatedPropsChange"

mccoyplayer/animation

commit 6e5376151f2915547584733c05a97256855ae149

Fix problem with drawer (#1665)
## Description

Fixes: https://github.com/software-mansion/react-native-reanimated/issues/1616
The issue was introduced in commit:  https://github.com/software-mansion/react-native-reanimated/commit/8638cdbd55f3250bc6d25588e27bb0f2ee44b603

In the commit mentioned above, we forgot to handle the case when the UIManager operations queue is empty and there is not an active batch. In such a case, we have to dispatch view updates ourselves. That's why I loosened the condition for the execution of the dispatch view updates.


## Test code and steps to reproduce

```JS
import React from 'react';
import { View, Text, Button } from 'react-native';
import { createDrawerNavigator } from '@react-navigation/drawer';
import { NavigationContainer } from '@react-navigation/native';

const Drawer = createDrawerNavigator();

function Screen({ route }) {
  const text = route.name;
  return (
    <View>
      <Text style={{ padding: 20 }}>{text}</Text>
      <Button
        title={`button from screen ${text}`}
        onPress={() => {
          alert(text);
        }}
      />
    </View>
  );
}

export default function MyDrawer() {
  return (
    <NavigationContainer>
      <Drawer.Navigator>
        <Drawer.Screen name="Feed" component={Screen} />
        <Drawer.Screen name="Article" component={Screen} />
      </Drawer.Navigator>
    </NavigationContainer>
  );
}

```

## Checklist

- [x] Included code example that can be used to test this change
- [x] Updated TS types
- [x] Added TS types tests
- [x] Added unit / integration tests
- [x] Updated documentation
- [ ] Ensured that CI passes

android/src/main/java/com/swmansion/reanimated/NodesManager.java
@@ -204,7 +204,8 @@ public class NodesManager implements EventDispatcherListener {
               new GuardedRunnable(mContext) {
                 @Override
                 public void runGuarded() {
-                  boolean shouldDispatchUpdates = trySynchronously && UIManagerReanimatedHelper.isOperationQueueEmpty(mUIImplementation);
+                  boolean queueWasEmpty = UIManagerReanimatedHelper.isOperationQueueEmpty(mUIImplementation);
+                  boolean shouldDispatchUpdates = trySynchronously && queueWasEmpty;
                   if (!shouldDispatchUpdates) {
                     semaphore.release();
                   }
@@ -215,8 +216,10 @@ public class NodesManager implements EventDispatcherListener {
                       mUIManager.updateView(op.mViewTag, shadowNode.getViewClass(), op.mNativeProps);
                     }
                   }
-                  if (shouldDispatchUpdates) {
+                  if (queueWasEmpty) {
                     mUIImplementation.dispatchViewUpdates(-1); // no associated batchId
+                  }
+                  if (shouldDispatchUpdates) {
                     semaphore.release();
                   }
                 }