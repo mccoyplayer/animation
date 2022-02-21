mccoyplayer/animation

commit ea74e27280c6fe3df4cd76205bc7e1e19e6c8194

Add style prop to every animated component (#2947)

react-native-reanimated.d.ts
@@ -146,10 +146,14 @@ declare module 'react-native-reanimated' {
               >;
     };
 
-    export type AnimateProps<P extends object> = {
-      [K in keyof P]: K extends 'style'
-        ? StyleProp<AnimateStyle<P[K]>>
-        : P[K] | AnimatedNode<P[K]>;
+    export type StylesOrDefault<T> = 'style' extends keyof T
+      ? T['style']
+      : Record<string, unknown>;
+
+    export type AnimateProps<P extends Record<string, unknown>> = {
+      [K in keyof P]: P[K] | AnimatedNode<P[K]>;
+    } & {
+      style?: StyleProp<AnimateStyle<StylesOrDefault<P>>>;
     } & {
       animatedProps?: Partial<AnimateProps<P>>;
       layout?:

react-native-reanimated-tests.tsx
@@ -81,7 +81,12 @@ const AnimatedFCWithRef = Animated.createAnimatedComponent(SomeFCWithRef);
 
 function CreateAnimatedComponentTest1() {
   const animatedProps = useAnimatedProps(() => ({ fill: 'blue' }));
-  return <AnimatedPath animatedProps={animatedProps} />;
+  return (
+    <AnimatedPath
+      animatedProps={animatedProps}
+      style={{ backgroundColor: 'red' }}
+    />
+  );
 }
 
 function CreateAnimatedComponentTest2() {

commit 48bf4b314115ae173b2d5bd3e7c8df04bb60ea24

Use `P extends object` in Animate props generic type (#3022)
## Description

Fixes regression introduced in #2947.

react-native-reanimated.d.ts
@@ -150,7 +150,7 @@ declare module 'react-native-reanimated' {
       ? T['style']
       : Record<string, unknown>;
 
-    export type AnimateProps<P extends Record<string, unknown>> = {
+    export type AnimateProps<P extends object> = {
       [K in keyof P]: P[K] | AnimatedNode<P[K]>;
     } & {
       style?: StyleProp<AnimateStyle<StylesOrDefault<P>>>;