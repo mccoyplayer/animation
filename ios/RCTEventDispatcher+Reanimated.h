mccoyplayer/animation

commit fa7995496fac698ed76c5b067d4eeca43b7080f4

Swizzle RCTEventDispatcher (#4576)
<!-- Thanks for submitting a pull request! We appreciate you spending
the time to work on these changes. Please follow the template so that
the reviewers can easily understand what the code changes affect. -->

## Summary

Reanimated needs to intercept events while still on the UI thread. To
achieve this on iOS, currently we overwrite the instance of
`RCTEventDispatcher` with an instance of `REAEventDispatcher` which
extends `RCTEventDispatcher` but overrides `sentEvent` method. We need
to overwrite event dispatcher as soon as possible, i.e. before native
modules are initialized, so that `RNGestureHandlerManager` holds an
already overwritten instance (see
[here](https://github.com/software-mansion/react-native-gesture-handler/blob/9a3b90df0df3e7f85bb06ae7946aea3be95f717c/ios/RNGestureHandlerManager.mm#L62)).
Currently, we call `REAInitializer` in `jsExecutorFactoryForBridge`
method. This PR removes some dirty initialization code and uses
swizzling instead to achieve the same effect.

<img width="1496" alt="screenshot"
src="https://github.com/software-mansion/react-native-reanimated/assets/20516055/86ed7bec-b40a-4f2a-a29a-9e72f511e77e">

## Test plan

1. Build Example app on iOS
2. Check scroll- and gesture-based examples

ios/RCTEventDispatcher+Reanimated.h
@@ -3,7 +3,9 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface REAEventDispatcher : RCTEventDispatcher
+@interface RCTEventDispatcher (Reanimated)
+
+- (void)reanimated_sentEvent:(id<RCTEvent>)event;
 
 @end
 

ios/RCTEventDispatcher+Reanimated.m
@@ -1,0 +1,28 @@
+#import <RNReanimated/RCTEventDispatcher+Reanimated.h>
+#import <React/RCTBridge+Private.h>
+#import <React/RCTEventDispatcher.h>
+#import <objc/message.h>
+
+@implementation RCTEventDispatcher (Reanimated)
+
+- (void)reanimated_sentEvent:(id<RCTEvent>)event
+{
+  // Pass the event to Reanimated
+  [[[self bridge] moduleForName:@"ReanimatedModule"] eventDispatcherWillDispatchEvent:event];
+
+  // Pass the event to React Native by calling the original method
+  [self reanimated_sentEvent:event];
+}
+
++ (void)load
+{
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    Class class = [self class];
+    Method originalMethod = class_getInstanceMethod(class, @selector(sendEvent:));
+    Method swizzledMethod = class_getInstanceMethod(class, @selector(reanimated_sentEvent:));
+    method_exchangeImplementations(originalMethod, swizzledMethod);
+  });
+}
+
+@end

ios/REAEventDispatcher.m
@@ -1,18 +1,0 @@
-#import <RNReanimated/REAEventDispatcher.h>
-#import <React/RCTBridge+Private.h>
-#import <React/RCTDefines.h>
-
-@implementation REAEventDispatcher
-
-- (void)sendEvent:(id<RCTEvent>)event
-{
-  [[[self bridge] moduleForName:@"ReanimatedModule"] eventDispatcherWillDispatchEvent:event];
-  [super sendEvent:event];
-}
-
-+ (NSString *)moduleName
-{
-  return NSStringFromClass([RCTEventDispatcher class]);
-}
-
-@end

ios/native/REAInitializer.mm
@@ -1,6 +1,5 @@
 #ifndef RCT_NEW_ARCH_ENABLED
 
-#import <RNReanimated/REAEventDispatcher.h>
 #import <RNReanimated/REAInitializer.h>
 #import <RNReanimated/REAUIManager.h>
 
@@ -13,20 +12,6 @@ void REAInitializer(RCTBridge *bridge)
   [reaUiManager setBridge:bridge];
   RCTUIManager *uiManager = reaUiManager;
   [bridge updateModuleWithInstance:uiManager];
-
-  [bridge moduleForClass:[RCTEventDispatcher class]];
-  RCTEventDispatcher *eventDispatcher = [REAEventDispatcher new];
-#if REACT_NATIVE_MINOR_VERSION >= 66
-  RCTCallableJSModules *callableJSModules = [RCTCallableJSModules new];
-  [bridge setValue:callableJSModules forKey:@"_callableJSModules"];
-  [callableJSModules setBridge:bridge];
-  [eventDispatcher setValue:callableJSModules forKey:@"_callableJSModules"];
-  [eventDispatcher setValue:bridge forKey:@"_bridge"];
-  [eventDispatcher initialize];
-#else
-  [eventDispatcher setBridge:bridge];
-#endif
-  [bridge updateModuleWithInstance:eventDispatcher];
 }
 
 #if REACT_NATIVE_MINOR_VERSION <= 71

commit 2165342a921e8f5bb6ebab3e02adbee3845c93b3

Fix typo in `reanimated_sendEvent` (#4588)
<!-- Thanks for submitting a pull request! We appreciate you spending
the time to work on these changes. Please follow the template so that
the reviewers can easily understand what the code changes affect. -->

## Summary

This PR fixes a typo introduced by me in #4576.

## Test plan

<!-- Provide a minimal but complete code snippet that can be used to
test out this change along with instructions how to run it and a
description of the expected behavior. -->

ios/RCTEventDispatcher+Reanimated.h
@@ -5,7 +5,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface RCTEventDispatcher (Reanimated)
 
-- (void)reanimated_sentEvent:(id<RCTEvent>)event;
+- (void)reanimated_sendEvent:(id<RCTEvent>)event;
 
 @end
 

ios/RCTEventDispatcher+Reanimated.m
@@ -5,13 +5,13 @@
 
 @implementation RCTEventDispatcher (Reanimated)
 
-- (void)reanimated_sentEvent:(id<RCTEvent>)event
+- (void)reanimated_sendEvent:(id<RCTEvent>)event
 {
   // Pass the event to Reanimated
   [[[self bridge] moduleForName:@"ReanimatedModule"] eventDispatcherWillDispatchEvent:event];
 
   // Pass the event to React Native by calling the original method
-  [self reanimated_sentEvent:event];
+  [self reanimated_sendEvent:event];
 }
 
 + (void)load
@@ -20,7 +20,7 @@
   dispatch_once(&onceToken, ^{
     Class class = [self class];
     Method originalMethod = class_getInstanceMethod(class, @selector(sendEvent:));
-    Method swizzledMethod = class_getInstanceMethod(class, @selector(reanimated_sentEvent:));
+    Method swizzledMethod = class_getInstanceMethod(class, @selector(reanimated_sendEvent:));
     method_exchangeImplementations(originalMethod, swizzledMethod);
   });
 }