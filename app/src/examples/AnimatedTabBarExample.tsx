software-mansion/react-native-reanimated

commit 68b92eee47af85e5b9baf6f224b3f778250bc91d

Add more eslint rules (#4672)
<!-- Thanks for submitting a pull request! We appreciate you spending
the time to work on these changes. Please follow the template so that
the reviewers can easily understand what the code changes affect. -->

## Summary

<!-- Explain the motivation for this PR. Include "Fixes #<number>" if
applicable. -->

* Rules for whole repo:
* '`@typescript-eslint/no-duplicate-type-constituents': 'error'` - means
that types like `number|number` are not allowed
  * `eqeqeq: 'error' - you can't compare with `==`, use `===` instead
  
* `'no-unreachable': 'error'` - no unreachable code, especially inside a
function, after return statement.
  
* Rules for examples in folder app:
  * `'react-native/no-unused-styles': 'error'`,
* `'react-native/no-raw-text': 'off'`, - this rule is very convenient,
but due to this bug
[LINK](https://github.com/Intellicode/eslint-plugin-react-native/issues/270)
we can't use it with CI 😠
  * `'no-inline-styles/no-inline-styles': 'error'`
* `'react-native/no-inline-styles': 'off'`, (this rule does not allow
variable styles with ternary operators, so I've used the one above)
  
  * `'react-native/no-single-element-style-arrays': 'error'`,
* Create separate .eslintrc.js config for tests with separate rules
  * `'jest/no-disabled-tests': 'warn'`,
  
  * `'jest/no-focused-tests': 'error'`,
  * `'jest/no-identical-title': 'error'`,
  * `'jest/prefer-to-have-length': 'warn'`,
  * `'jest/valid-expect': 'error'`,

## Test plan

<!-- Provide a minimal but complete code snippet that can be used to
test out this change along with instructions how to run it and a
description of the expected behavior. -->
CI check pass ✅

---------

Co-authored-by: Aleksandra Cynk <aleksandracynk@swmansion.com>
Co-authored-by: Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>

app/src/examples/AnimatedTabBarExample.tsx
@@ -79,6 +79,10 @@ const styles = StyleSheet.create({
     height: 64,
     zIndex: 2,
   },
+  tabs: {
+    position: 'absolute',
+    left: -tabWidth,
+  },
   activeIcon: {
     backgroundColor: 'white',
     width: 40,
@@ -86,6 +90,16 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  circleIcon: {
+    position: 'absolute',
+    width: width,
+    top: -8,
+    left: width / 2,
+    height: 64,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 5,
   },
 });
 
@@ -140,7 +154,7 @@ type ActiveIconProps = {
   activeIndex: Animated.SharedValue<number>;
   width: number;
 };
-function ActiveIcon({ item, index, activeIndex, width }: ActiveIconProps) {
+function ActiveIcon({ item, index, activeIndex }: ActiveIconProps) {
   const circleIconStyle = useAnimatedStyle(() => {
     const isActive = index === activeIndex.value;
     const yOffset = isActive ? 0 : 80;
@@ -154,20 +168,7 @@ function ActiveIcon({ item, index, activeIndex, width }: ActiveIconProps) {
   });
 
   return (
-    <Animated.View
-      style={[
-        {
-          position: 'absolute',
-          width: width,
-          top: -8,
-          left: width / 2,
-          height: 64,
-          justifyContent: 'center',
-          alignItems: 'center',
-          zIndex: 5,
-        },
-        circleIconStyle,
-      ]}>
+    <Animated.View style={[styles.circleIcon, circleIconStyle]}>
       <View style={styles.activeIcon}>
         <FontAwesomeIcon icon={item} color="black" size={25} />
       </View>
@@ -192,8 +193,7 @@ function Bar() {
 
   return (
     <View style={styles.container}>
-      <Animated.View
-        style={[{ position: 'absolute', left: -tabWidth }, indicatorStyle]}>
+      <Animated.View style={[styles.tabs, indicatorStyle]}>
         {tabs.map((tab, index) => (
           <ActiveIcon
             index={index}

app/src/examples/ExtrapolationExample.tsx
@@ -86,13 +86,7 @@ export default function ExtrapolationExample(): React.ReactElement {
   return (
     <>
       <PanGestureHandler onGestureEvent={gestureHandler}>
-        <Animated.View
-          style={[
-            styles.circle,
-            { zIndex: 1, backgroundColor: '#001a72' },
-            stylez,
-          ]}
-        />
+        <Animated.View style={[styles.circle, stylez]} />
       </PanGestureHandler>
       <Animated.View style={[styles.circle, button1Style]} />
       <Animated.View style={[styles.circle, button2Style]} />
@@ -109,6 +103,7 @@ const styles = StyleSheet.create({
     top: 400,
     alignSelf: 'center',
     borderRadius: 25,
-    backgroundColor: 'red',
+    zIndex: 1,
+    backgroundColor: '#001a72',
   },
 });

app/src/examples/SharedElementTransitions/RestoreState.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
+import { Button, View, StyleSheet } from 'react-native';
 import { ParamListBase } from '@react-navigation/native';
 import {
   createNativeStackNavigator,
@@ -11,16 +11,10 @@ const Stack = createNativeStackNavigator();
 
 function Screen1({ navigation }: NativeStackScreenProps<ParamListBase>) {
   return (
-    <View style={{ flex: 1, marginTop: 50, marginLeft: 50 }}>
-      <Animated.ScrollView style={{ marginTop: 50, marginLeft: 50 }}>
+    <View style={[styles.flexOne, styles.margins]}>
+      <Animated.ScrollView style={styles.margins}>
         <Animated.View
-          style={{
-            width: 50,
-            height: 100,
-            backgroundColor: 'red',
-            marginTop: 50,
-            marginLeft: 50,
-          }}
+          style={styles.redBox}
           sharedTransitionTag="sharedElement"
         />
         <Button
@@ -34,14 +28,9 @@ function Screen1({ navigation }: NativeStackScreenProps<ParamListBase>) {
 
 function Screen2({ navigation }: NativeStackScreenProps<ParamListBase>) {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.flexOne}>
       <Animated.View
-        style={{
-          width: '100%',
-          height: 200,
-          backgroundColor: 'green',
-          marginTop: 200,
-        }}
+        style={styles.greenBox}
         sharedTransitionTag="sharedElement"
       />
       <Button
@@ -55,7 +44,7 @@ function Screen2({ navigation }: NativeStackScreenProps<ParamListBase>) {
 
 function Screen3({ navigation }: NativeStackScreenProps<ParamListBase>) {
   return (
-    <View style={{ flex: 1, marginTop: 50 }}>
+    <View style={styles.container}>
       <Button
         title="go to screen1"
         onPress={() => navigation.navigate('Screen1')}
@@ -92,3 +81,30 @@ export default function RestoreStateExample() {
     </Stack.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  flexOne: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    marginTop: 50,
+  },
+  margins: {
+    marginTop: 50,
+    marginLeft: 50,
+  },
+  redBox: {
+    width: 50,
+    height: 100,
+    backgroundColor: 'red',
+    marginTop: 50,
+    marginLeft: 50,
+  },
+  greenBox: {
+    width: '100%',
+    height: 200,
+    backgroundColor: 'green',
+    marginTop: 200,
+  },
+});

.eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   extends: [
     'standard',
     'plugin:@typescript-eslint/recommended',
@@ -34,9 +37,8 @@ module.exports = {
     ],
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-var-requires': 'warn',
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
-      { prefer: 'type-imports' },
-    ],
+    '@typescript-eslint/no-duplicate-type-constituents': 'error',
+    eqeqeq: 'error',
+    'no-unreachable': 'error',
   },
 };

__tests__/.eslintrc.js
@@ -1,0 +1,28 @@
+module.exports = {
+  root: true,
+  extends: [
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:import/typescript',
+  ],
+  plugins: ['jest', 'react-native'],
+  env: {
+    'react-native/react-native': true,
+    'jest/globals': true,
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
+};

__tests__/plugin.test.ts
@@ -1023,8 +1023,8 @@ describe('babel plugin', () => {
   it('is indempotent for common cases', () => {
     function resultIsIdempotent(input: string) {
       const firstResult = runPlugin(input).code;
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const secondResult = runPlugin(firstResult!).code;
+      assert(firstResult);
+      const secondResult = runPlugin(firstResult).code;
       return firstResult === secondResult;
     }
 

app/.eslintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['eslint-plugin-no-inline-styles'],
   rules: {
+    'no-inline-styles/no-inline-styles': 'error',
     'react-native/no-inline-styles': 'off',
     '@typescript-eslint/no-shadow': 'off',
+    'react-native/no-unused-styles': 'error',
+    'react-native/no-raw-text': 'off',
+    'react-native/no-single-element-style-arrays': 'error',
   },
 };

app/src/examples/AnimatedStyleUpdateExample.tsx
@@ -4,7 +4,7 @@ import Animated, {
   useAnimatedStyle,
   Easing,
 } from 'react-native-reanimated';
-import { View, Button } from 'react-native';
+import { View, Button, StyleSheet } from 'react-native';
 import React from 'react';
 
 export default function AnimatedStyleUpdateExample(): React.ReactElement {
@@ -22,17 +22,8 @@ export default function AnimatedStyleUpdateExample(): React.ReactElement {
   });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: 'column',
-      }}>
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: 'black', margin: 30 },
-          style,
-        ]}
-      />
+    <View style={styles.container}>
+      <Animated.View style={[styles.box, style]} />
       <Button
         title="toggle"
         onPress={() => {
@@ -42,3 +33,16 @@ export default function AnimatedStyleUpdateExample(): React.ReactElement {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  box: {
+    width: 100,
+    height: 80,
+    backgroundColor: 'black',
+    margin: 30,
+  },
+});

mccoyplayer/animation

commit 885dc72961a9ff3ae0cd942730cf04ead3755b87

Fix examples (#4806)
## Summary

This PR fixes some regression in our examples introduced in
https://github.com/software-mansion/react-native-reanimated/pull/4672
PR.

## What was wrong?

|Before|After|
|---|---|
| <video
src="https://github.com/software-mansion/react-native-reanimated/assets/36106620/a711194f-d699-4445-9d54-c7e80e373a9e"
/> | <video
src="https://github.com/software-mansion/react-native-reanimated/assets/36106620/9a81ff19-d5cd-45f5-9f32-a12b3e6446f4"
/> |

|Before|After|
|---|---|
| <video
src="https://github.com/software-mansion/react-native-reanimated/assets/36106620/948a6b28-370b-4da1-a0d7-9bb757026f9f"
/> | <video
src="https://github.com/software-mansion/react-native-reanimated/assets/36106620/64bd05fa-1549-41f7-bffb-7a22d530cb66"
/> |

|Before|After|
|---|---|
| <video
src="https://github.com/software-mansion/react-native-reanimated/assets/36106620/28fa7c95-3f24-44b7-847a-6b6b6dfff964"
/> | <video
src="https://github.com/software-mansion/react-native-reanimated/assets/36106620/8050de48-5490-4491-8fd5-f9781ba17c90"
/> |

|Before|After|
|---|---|
| <video
src="https://github.com/software-mansion/react-native-reanimated/assets/36106620/31892ece-663b-4f98-8244-f74b84d868f0"
/> | <video
src="https://github.com/software-mansion/react-native-reanimated/assets/36106620/abe1c2f5-2c50-49c7-810f-98b7353a31a6"
/> |

app/src/examples/AnimatedTabBarExample.tsx
@@ -93,9 +93,9 @@ const styles = StyleSheet.create({
   },
   circleIcon: {
     position: 'absolute',
-    width: width,
+    width: tabWidth,
     top: -8,
-    left: width / 2,
+    left: tabWidth / 2,
     height: 64,
     justifyContent: 'center',
     alignItems: 'center',

app/src/examples/ExtrapolationExample.tsx
@@ -86,7 +86,7 @@ export default function ExtrapolationExample(): React.ReactElement {
   return (
     <>
       <PanGestureHandler onGestureEvent={gestureHandler}>
-        <Animated.View style={[styles.circle, stylez]} />
+        <Animated.View style={[styles.circle, styles.topCircle, stylez]} />
       </PanGestureHandler>
       <Animated.View style={[styles.circle, button1Style]} />
       <Animated.View style={[styles.circle, button2Style]} />
@@ -96,6 +96,10 @@ export default function ExtrapolationExample(): React.ReactElement {
 }
 
 const styles = StyleSheet.create({
+  topCircle: {
+    zIndex: 1,
+    backgroundColor: '#001a72',
+  },
   circle: {
     width: 50,
     height: 50,
@@ -103,7 +107,6 @@ const styles = StyleSheet.create({
     top: 400,
     alignSelf: 'center',
     borderRadius: 25,
-    zIndex: 1,
-    backgroundColor: '#001a72',
+    backgroundColor: 'red',
   },
 });

app/src/examples/MatrixTransform.tsx
@@ -32,6 +32,8 @@ export default function MatrixTransform() {
   });
 
   const handlePress = React.useCallback(() => {
+    currentTransformIndex.current++;
+    currentTransformIndex.current %= TRANSFORM_MATRICES.length;
     matrix.value = withSpring(
       TRANSFORM_MATRICES[currentTransformIndex.current],
       springConfig
@@ -41,9 +43,6 @@ export default function MatrixTransform() {
       [...TRANSFORM_MATRICES[currentTransformIndex.current], 0],
       springConfig
     );
-
-    currentTransformIndex.current += 1;
-    currentTransformIndex.current %= TRANSFORM_MATRICES.length;
   }, [matrix, matrix2, currentTransformIndex]);
 
   return (

app/src/examples/SharedElementTransitions/RestoreState.tsx
@@ -61,7 +61,7 @@ export default function RestoreStateExample() {
   return (
     <Stack.Navigator
       screenOptions={{
-        animation: 'none',
+        animation: 'fade',
       }}>
       <Stack.Screen
         name="Screen1"