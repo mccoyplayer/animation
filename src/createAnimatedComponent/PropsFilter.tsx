software-mansion/react-native-reanimated

commit b7aa178f75b0e6611e86f16ea331669acc4b2aba

Reduce file createAnimatedComponent - part3 (#5102)
<!-- Thanks for submitting a pull request! We appreciate you spending
the time to work on these changes. Please follow the template so that
the reviewers can easily understand what the code changes affect. -->

## Summary
File `createAnimatedComponent.tsx` contains our most important logic. It
is already very big and since in the future we want to support
${\textcolor{teal}{\textsf{inline Styles}}}$ this file may grow a lot.
Thats why we should make it clear and easy to rid - for example by
splitting it across many small files.

This refactor will be made in smaller parts.

### Part 1
[LINK](https://github.com/software-mansion/react-native-reanimated/pull/5053)
* Create folder 📁 `createAnimatedComponent` 
* Move all files used only in `createAnimatedComponent.tsx` into that
folder. Move `createAnimatedComponent.tsx` as well.
* Extract all util function into separate file.
### Part 2
([LINK](https://github.com/software-mansion/react-native-reanimated/pull/5003))
* Create class `inlinePropUpdater.tsx` which handles logic related with
inline styles.
### Part 3 ($\textcolor{green}{\textsf{This PR}}$)
This is not fixed yet, but I suggest we should:
* Move function `_filterNonAnimatedProps` away (70 lines of code)

## Test plan

Run the app, nothing explodes 🤯 
<!-- Provide a minimal but complete code snippet that can be used to
test out this change along with instructions how to run it and a
description of the expected behavior. -->

---------

Co-authored-by: Aleksandra Cynk <aleksandra.cynk@swmansion.com>

src/createAnimatedComponent/PropsFilter.tsx
@@ -1,0 +1,93 @@
+'use strict';
+
+import type { StyleProps, SharedValue } from '../reanimated2';
+import { isSharedValue } from '../reanimated2';
+import { isChromeDebugger } from '../reanimated2/PlatformChecker';
+import WorkletEventHandler from '../reanimated2/WorkletEventHandler';
+import { initialUpdaterRun } from '../reanimated2/animation';
+import { hasInlineStyles, getInlineStyle } from './InlinePropManager';
+import type {
+  AnimatedComponentProps,
+  AnimatedProps,
+  InitialComponentProps,
+} from './utils';
+import { flattenArray, has } from './utils';
+import { StyleSheet } from 'react-native';
+
+function dummyListener() {
+  // empty listener we use to assign to listener properties for which animated
+  // event is used.
+}
+
+export class PropsFilter {
+  private _initialStyle = {};
+  private _isFirstRender = true;
+
+  public filterNonAnimatedProps(
+    inputProps: AnimatedComponentProps<InitialComponentProps>
+  ): Record<string, unknown> {
+    const props: Record<string, unknown> = {};
+    for (const key in inputProps) {
+      const value = inputProps[key];
+      if (key === 'style') {
+        const styleProp = inputProps.style;
+        const styles = flattenArray<StyleProps>(styleProp ?? []);
+        const processedStyle: StyleProps = styles.map((style) => {
+          if (style && style.viewDescriptors) {
+            // this is how we recognize styles returned by useAnimatedStyle
+            style.viewsRef.add(this);
+            if (this._isFirstRender) {
+              this._initialStyle = {
+                ...style.initial.value,
+                ...this._initialStyle,
+                ...initialUpdaterRun<StyleProps>(style.initial.updater),
+              };
+            }
+            return this._initialStyle;
+          } else if (hasInlineStyles(style)) {
+            return getInlineStyle(style, this._isFirstRender);
+          } else {
+            return style;
+          }
+        });
+        props[key] = StyleSheet.flatten(processedStyle);
+      } else if (key === 'animatedProps') {
+        const animatedProp = inputProps.animatedProps as Partial<
+          AnimatedComponentProps<AnimatedProps>
+        >;
+        if (animatedProp.initial !== undefined) {
+          Object.keys(animatedProp.initial.value).forEach((key) => {
+            props[key] = animatedProp.initial?.value[key];
+            animatedProp.viewsRef?.add(this);
+          });
+        }
+      } else if (
+        has('current', value) &&
+        value.current instanceof WorkletEventHandler
+      ) {
+        if (value.current.eventNames.length > 0) {
+          value.current.eventNames.forEach((eventName) => {
+            props[eventName] = has('listeners', value.current)
+              ? (value.current.listeners as Record<string, unknown>)[eventName]
+              : dummyListener;
+          });
+        } else {
+          props[key] = dummyListener;
+        }
+      } else if (isSharedValue(value)) {
+        if (this._isFirstRender) {
+          props[key] = (value as SharedValue<any>).value;
+        }
+      } else if (key !== 'onGestureHandlerStateChange' || !isChromeDebugger()) {
+        props[key] = value;
+      }
+    }
+    return props;
+  }
+
+  public onRender() {
+    if (this._isFirstRender) {
+      this._isFirstRender = false;
+    }
+  }
+}

src/createAnimatedComponent/createAnimatedComponent.tsx
@@ -5,10 +5,9 @@ import type {
   ComponentType,
   FunctionComponent,
   MutableRefObject,
-  Ref,
 } from 'react';
 import React from 'react';
-import { findNodeHandle, Platform, StyleSheet } from 'react-native';
+import { findNodeHandle, Platform } from 'react-native';
 import WorkletEventHandler from '../reanimated2/WorkletEventHandler';
 import '../reanimated2/layoutReanimation/animationsManager';
 import invariant from 'invariant';
@@ -22,42 +21,25 @@ import {
   SharedTransition,
   LayoutAnimationType,
 } from '../reanimated2/layoutReanimation';
-import type {
-  SharedValue,
-  StyleProps,
-  ShadowNodeWrapper,
-} from '../reanimated2/commonTypes';
+import type { StyleProps, ShadowNodeWrapper } from '../reanimated2/commonTypes';
 import { getShadowNodeWrapperFromRef } from '../reanimated2/fabricUtils';
-import { isSharedValue } from '../reanimated2/utils';
 import { removeFromPropsRegistry } from '../reanimated2/PropsRegistry';
-import {
-  getReduceMotionFromConfig,
-  initialUpdaterRun,
-} from '../reanimated2/animation/util';
+import { getReduceMotionFromConfig } from '../reanimated2/animation/util';
 import { maybeBuild } from '../animationBuilder';
 import type { AnimateProps } from '../reanimated2';
 import { JSPropUpdater } from './JSPropUpdater';
-import type { AnimatedComponentProps, AnimatedProps } from './utils';
+import type {
+  AnimatedComponentProps,
+  AnimatedProps,
+  InitialComponentProps,
+} from './utils';
 import { has, flattenArray } from './utils';
 import setAndForwardRef from './setAndForwardRef';
-import {
-  isChromeDebugger,
-  isJest,
-  isWeb,
-  shouldBeUseWeb,
-} from '../reanimated2/PlatformChecker';
-import {
-  InlinePropManager,
-  getInlineStyle,
-  hasInlineStyles,
-} from './InlinePropManager';
+import { isJest, isWeb, shouldBeUseWeb } from '../reanimated2/PlatformChecker';
+import { InlinePropManager } from './InlinePropManager';
+import { PropsFilter } from './PropsFilter';
 
 const IS_WEB = isWeb();
-
-function dummyListener() {
-  // empty listener we use to assign to listener properties for which animated
-  // event is used.
-}
 
 function onlyAnimatedStyles(styles: StyleProps[]): StyleProps[] {
   return styles.filter((style) => style?.viewDescriptors);
@@ -82,11 +64,6 @@ interface ComponentRef extends Component {
   setNativeProps?: (props: Record<string, unknown>) => void;
   getScrollableNode?: () => ComponentRef;
   getAnimatableRef?: () => ComponentRef;
-}
-
-interface InitialComponentProps extends Record<string, unknown> {
-  ref?: Ref<Component>;
-  collapsable?: boolean;
 }
 
 export function createAnimatedComponent<P extends object>(
@@ -117,11 +94,11 @@ export function createAnimatedComponent(
     _viewTag = -1;
     _isFirstRender = true;
     animatedStyle: { value: StyleProps } = { value: {} };
-    initialStyle = {};
     _component: ComponentRef | HTMLElement | null = null;
     _sharedElementTransition: SharedTransition | null = null;
     _JSPropUpdater = new JSPropUpdater();
     _InlinePropManager = new InlinePropManager();
+    _PropsFilter = new PropsFilter();
     static displayName: string;
 
     constructor(props: AnimatedComponentProps<InitialComponentProps>) {
@@ -465,81 +442,12 @@ export function createAnimatedComponent(
       },
     });
 
-    _filterNonAnimatedProps(
-      inputProps: AnimatedComponentProps<InitialComponentProps>
-    ): Record<string, unknown> {
-      const props: Record<string, unknown> = {};
-      for (const key in inputProps) {
-        const value = inputProps[key];
-        if (key === 'style') {
-          const styleProp = inputProps.style;
-          const styles = flattenArray<StyleProps>(styleProp ?? []);
-          const processedStyle: StyleProps = styles.map((style) => {
-            if (style && style.viewDescriptors) {
-              // this is how we recognize styles returned by useAnimatedStyle
-              style.viewsRef.add(this);
-              if (this._isFirstRender) {
-                this.initialStyle = {
-                  ...style.initial.value,
-                  ...this.initialStyle,
-                  ...initialUpdaterRun<StyleProps>(style.initial.updater),
-                };
-              }
-              return this.initialStyle;
-            } else if (hasInlineStyles(style)) {
-              return getInlineStyle(style, this._isFirstRender);
-            } else {
-              return style;
-            }
-          });
-          props[key] = StyleSheet.flatten(processedStyle);
-        } else if (key === 'animatedProps') {
-          const animatedProp = inputProps.animatedProps as Partial<
-            AnimatedComponentProps<AnimatedProps>
-          >;
-          if (animatedProp.initial !== undefined) {
-            Object.keys(animatedProp.initial.value).forEach((key) => {
-              props[key] = animatedProp.initial?.value[key];
-              animatedProp.viewsRef?.add(this);
-            });
-          }
-        } else if (
-          has('current', value) &&
-          value.current instanceof WorkletEventHandler
-        ) {
-          if (value.current.eventNames.length > 0) {
-            value.current.eventNames.forEach((eventName) => {
-              props[eventName] = has('listeners', value.current)
-                ? (value.current.listeners as Record<string, unknown>)[
-                    eventName
-                  ]
-                : dummyListener;
-            });
-          } else {
-            props[key] = dummyListener;
-          }
-        } else if (isSharedValue(value)) {
-          if (this._isFirstRender) {
-            props[key] = (value as SharedValue<any>).value;
-          }
-        } else if (
-          key !== 'onGestureHandlerStateChange' ||
-          !isChromeDebugger()
-        ) {
-          props[key] = value;
-        }
-      }
-      return props;
-    }
-
     render() {
-      const props = this._filterNonAnimatedProps(this.props);
+      const props = this._PropsFilter.filterNonAnimatedProps(this.props);
+      this._PropsFilter.onRender();
+
       if (isJest()) {
         props.animatedStyle = this.animatedStyle;
-      }
-
-      if (this._isFirstRender) {
-        this._isFirstRender = false;
       }
 
       const platformProps = Platform.select({

src/createAnimatedComponent/utils.ts
@@ -44,6 +44,11 @@ export type AnimatedComponentProps<P extends Record<string, unknown>> = P & {
 
 type NestedArray<T> = T | NestedArray<T>[];
 
+export interface InitialComponentProps extends Record<string, unknown> {
+  ref?: Ref<Component>;
+  collapsable?: boolean;
+}
+
 export function flattenArray<T>(array: NestedArray<T>): T[] {
   if (!Array.isArray(array)) {
     return [array];

mccoyplayer/animation

commit 27b5960a65c933460cee5a98f4dad9c6aeeeb6ad

Fix `this` context on Web (#5125)
## Summary

This PR fixes a regression introduced in
https://github.com/software-mansion/react-native-reanimated/pull/5102.
The `filterNonAnimatedProps` method stores `this` in some registries,
after the relocation of this function to another class `this` no longer
points to a component.

## Test plan

Run web example and run any animation

src/createAnimatedComponent/PropsFilter.tsx
@@ -24,8 +24,10 @@ export class PropsFilter {
   private _isFirstRender = true;
 
   public filterNonAnimatedProps(
-    inputProps: AnimatedComponentProps<InitialComponentProps>
+    component: React.Component<unknown, unknown>
   ): Record<string, unknown> {
+    const inputProps =
+      component.props as AnimatedComponentProps<InitialComponentProps>;
     const props: Record<string, unknown> = {};
     for (const key in inputProps) {
       const value = inputProps[key];
@@ -35,7 +37,7 @@ export class PropsFilter {
         const processedStyle: StyleProps = styles.map((style) => {
           if (style && style.viewDescriptors) {
             // this is how we recognize styles returned by useAnimatedStyle
-            style.viewsRef.add(this);
+            style.viewsRef.add(component);
             if (this._isFirstRender) {
               this._initialStyle = {
                 ...style.initial.value,
@@ -58,7 +60,7 @@ export class PropsFilter {
         if (animatedProp.initial !== undefined) {
           Object.keys(animatedProp.initial.value).forEach((key) => {
             props[key] = animatedProp.initial?.value[key];
-            animatedProp.viewsRef?.add(this);
+            animatedProp.viewsRef?.add(component);
           });
         }
       } else if (

src/createAnimatedComponent/createAnimatedComponent.tsx
@@ -443,7 +443,7 @@ export function createAnimatedComponent(
     });
 
     render() {
-      const props = this._PropsFilter.filterNonAnimatedProps(this.props);
+      const props = this._PropsFilter.filterNonAnimatedProps(this);
       this._PropsFilter.onRender();
 
       if (isJest()) {